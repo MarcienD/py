97baf71ee6595cf4f2e547b288c91a3d486e98fb
MarcienD <marcien.dutkowskis@gmail.com>
2023-04-02 21:36:24 +0200

    :

--- Pobranie_logow.py
+++ Pobranie_logow.py
@@ -1,7 +1,5 @@
-import copy
-import time
-from collections import Counter
 from datetime import datetime
+import copy
 
 o = open("wejscie.txt")
 logi = o.read()
@@ -20,7 +18,6 @@ daty = []
 czasy = []
 temperatury = []
 
-
 x = 0
 for i in logi:
     daty.append(logi_pomocnicza[x][0])
@@ -35,7 +32,6 @@ for i in logi:
 
 def czy_wadliwe():
     wadliwe = []
-
     i = 0
     for data in daty:
         if daty_edit[i].isdigit():
@@ -62,33 +58,41 @@ def czy_wadliwe():
 
     wadliwe_logi = []
     [wadliwe_logi.append(log) for log in wadliwe if log not in wadliwe_logi]
-    '''wadliwe_print = []
-    for wiersz in wadliwe_logi:
-        wadliwe_print.append(wiersz)
-    for wiersz in wadliwe_print:
-        print('"', ' '.join(wiersz), '"')'''
-
     return wadliwe_logi
 
-
-wadliwe_logi = czy_wadliwe()
-procent_wadliwych_logow = 100.0
-procent_wadliwych_logow = round((len(wadliwe_logi)/len(logi) * 100), 1)
+def licz_procent_wadliwych():
+    wadliwe_logi = czy_wadliwe()
+    procent_wadliwych_logow = 100.0
+    procent_wadliwych_logow = round((len(wadliwe_logi) / len(logi) * 100), 1)
+    return procent_wadliwych_logow
 
 
 def zwroc_prawidlowe():
+    wadliwe_logi = czy_wadliwe()
     for x in wadliwe_logi:
         logi.remove(x)
     return logi
 
 def czas_trwania():
+    dane = zwroc_prawidlowe()
     data_czas = []
-    for x in logi:
-        for n in x:
+    for x in dane:
+        for c in x:
             x.pop()
         data_czas.append(x)
-        x.append(n)
-    
+        x.append(c)
+    if len(data_czas) > 1:
+        start = data_czas[0][0], data_czas[0][1]
+        stop = data_czas[len(data_czas) - 1][0], data_czas[len(data_czas) - 1][1]
+        start = ' '.join([str(x) for x in start])
+        stop = ' '.join([str(x) for x in stop])
+        start = datetime.strptime(start, '%Y-%m-%d %H:%M')
+        stop = datetime.strptime(stop, '%Y-%m-%d %H:%M')
+        czas_raportu = (stop - start).total_seconds() / 60
+    else:
+        czas_raportu = 0
+    return czas_raportu
+
 def slownik_temperatur():
    slownik = {"max": None, "min": None, "srednia": None}
    temperatury = []
@@ -98,7 +102,14 @@ def slownik_temperatur():
        n += 1
    temperatury = [t.strip('C') for t in temperatury]
    temperatury_float = list(map(float, temperatury))
-   slownik["max"] = max(temperatury_float)
-   slownik["min"] = min(temperatury_float)
-   slownik["srednia"] = round(sum(temperatury_float) / len(temperatury_float), 1)
-   return slownik
+   slownik["max"] = str(max(temperatury_float))
+   slownik["min"] = str(min(temperatury_float))
+   slownik["srednia"] = str(round(sum(temperatury_float) / len(temperatury_float), 1))
+   ##return slownik
+
+def licz_okresy_przegrzania():
+    liczba_okresow_przegrzania = 0
+    for log in logi:
+        if float(log[2].strip('C')) > 100:
+            liczba_okresow_przegrzania += 1
+    return liczba_okresow_przegrzania
--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import Pobranie_logow
 
-x = Pobranie_logow.zwroc_prawidlowe()
+
+x = Pobranie_logow.czy_wadliwe()
 print(x)
-n = Pobranie_logow.slownik_temperatur()
-print(n)
-i = Pobranie_logow.czas_trwania()
+y = Pobranie_logow.licz_procent_wadliwych()
+print(y)
+z = Pobranie_logow.zwroc_prawidlowe()
+print(z)
+
+u = Pobranie_logow.slownik_temperatur()
+print(u)
+i = Pobranie_logow.licz_okresy_przegrzania()
 print(i)
 
 
 
-
-
-
-def generuj_raport():
-    print('Wadliwe logi')
-    print('"procent_wadliwych_logów":" "')
-    print('"czas_trwania_raportu": ')
-    print('"temperatrura": {\n\t"max": ,\n\t"min": \n\t"srednia": ')
-
-
-
-
-
-##def odczyt(sciezka):
-##    x = open(sciezka)
-##    wszystkie_linie = x.readlines()
-##    for linie in wszystkie_linie:
-## a = "C:\\Users\\dutko\Downloads\\Uczelnia\\Uczelnia Python\\projekt\\wejscie.txt"
-## print(odczyt(a))
